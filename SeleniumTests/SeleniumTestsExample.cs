a612a5cd02f625a02dd9a04a49dcf7de55f5d0b7
Michal Slavík <xslavik@fi.muni.cz>
2022-04-27 17:11:24 +0200

    test scenario test

--- SeleniumTests/SeleniumTestsExample.cs
+++ SeleniumTests/SeleniumTestsExample.cs
@@ -12,7 +12,7 @@ namespace SeleniumTests
         public void startBrowser()
         {
             // TODO: Place here path to you chrome driver
-            driver = new ChromeDriver("");
+            driver = new ChromeDriver("D:\\Documents\\pv260\\chromedriver_win32");
         }
 
         [Test]
@@ -21,6 +21,36 @@ namespace SeleniumTests
             driver.Url = "https://www.saucedemo.com";
 
             // TODO: Place your test code here
+            driver.FindElement(By.Name("user-name")).SendKeys("standard_user");
+            driver.FindElement(By.Name("password")).SendKeys("secret_sauce");
+            driver.FindElement(By.Name("login-button")).Submit();
+            
+            driver.FindElement(By.Name("add-to-cart-sauce-labs-backpack")).Click();
+            Assert.AreEqual("1", driver.FindElement(By.ClassName("shopping_cart_badge")).Text);
+            
+            driver.FindElement(By.Name("add-to-cart-sauce-labs-bike-light")).Click();
+            Assert.AreEqual("2", driver.FindElement(By.ClassName("shopping_cart_badge")).Text);
+            
+            driver.FindElement(By.ClassName("shopping_cart_link")).Click();
+            
+            Assert.True(driver.FindElement(By.PartialLinkText("Sauce Labs Backpack")).Displayed);
+            Assert.True(driver.FindElement(By.PartialLinkText("Sauce Labs Bike Light")).Displayed);
+            
+            driver.FindElement(By.Name("checkout")).Click();
+
+            driver.FindElement(By.Name("firstName")).SendKeys("Michal");
+            driver.FindElement(By.Name("lastName")).SendKeys("Slavik");
+            driver.FindElement(By.Name("postalCode")).SendKeys("61300");
+
+
+            driver.FindElement(By.Name("continue")).Click();
+            
+            Assert.True(driver.FindElement(By.PartialLinkText("Sauce Labs Backpack")).Displayed);
+            Assert.True(driver.FindElement(By.PartialLinkText("Sauce Labs Bike Light")).Displayed);
+            
+            driver.FindElement(By.Name("finish")).Click();
+            
+            Assert.True(driver.FindElement(By.ClassName("pony_express")).Displayed);
         }
 
         [TearDown]
